3e4b2f4448ca21ee8313e669597c27e5c41d7046
nordine-frizi <nordine.frizi@laplateforme.io>
2026-04-22 14:57:20 +0200

    Jour 1 - job05 afficherJoursSemaine

---
+++ job05.js
@@ -0,0 +1,8 @@
+function afficherJoursSemaine() {
+    let JoursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    
+    for (let i = 0; i < JoursSemaines.length; i++) {
+        console.log(JoursSemaines[i]);
+    }
+}
+afficherJoursSemaines();
---
+++ jour-1/job05.js
@@ -0,0 +1,9 @@
+function afficherJoursSemaine() {
+  let JoursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+  for (let i = 0; i < JoursSemaines.length; i++) {
+    console.log(JoursSemaines[i]);
+  }
+}
+
+afficherJoursSemaine();
